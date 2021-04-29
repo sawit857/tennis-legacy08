3e1ecf9d30b1d01f31d98c20b2b2839fd6910af0
sawit857 <sawit.w@gmail.com>
2021-04-29 12:13:06 +0700

    modify method

--- src/main/java/com/summitthai/demo/TennisMain.java
+++ src/main/java/com/summitthai/demo/TennisMain.java
@@ -11,45 +11,67 @@ public class TennisMain {
 		
 		TennisMain main = new TennisMain();
 		
-		main.countScore("A");
-		System.out.println(String.format("scorePlayerA: %s, scorePlayerB: %s", main.showScore(scorePlayerA), main.showScore(scorePlayerB)));
-		main.countScore("B");
-		System.out.println(String.format("scorePlayerA: %s, scorePlayerB: %s", main.showScore(scorePlayerA), main.showScore(scorePlayerB)));
-		main.countScore("B");
-		System.out.println(String.format("scorePlayerA: %s, scorePlayerB: %s", main.showScore(scorePlayerA), main.showScore(scorePlayerB)));
-		main.countScore("B");
-		System.out.println(String.format("scorePlayerA: %s, scorePlayerB: %s", main.showScore(scorePlayerA), main.showScore(scorePlayerB)));
+		main.playAAddScore();
+		main.playBAddScore();
+		
+		System.out.println(main.score());
+				
 	}
 	
-	private void countScore(String playerWin) {
-		
-		if (playerWin.contentEquals("A")) {			 
-			if (scorePlayerA >= 30) {
-				scorePlayerA += 10;
-			} else {
-				scorePlayerA += 15;	
-			}
-		} else if (playerWin.contentEquals("B")) {
-			if (scorePlayerB >= 30) {
-				scorePlayerB += 10;
-			} else {
-				scorePlayerB += 15;	
-			}
-		} 
-	}
+	public String score() {
+        if (scorePlayerA == 0 && scorePlayerB == 0) {
+            return "love all";
+        }
+        if (scorePlayerA == 1 && scorePlayerB == 0) {
+            return "fifteen love";
+        }
+        if (scorePlayerA == 2 && scorePlayerB == 0) {
+            return "thirty love";
+        }
+        if (scorePlayerA == 3 && scorePlayerB == 0) {
+            return "forty love";
+        }
+        if (scorePlayerA == 0 && scorePlayerB == 1) {
+            return "love fifteen";
+        }
+        if (scorePlayerA == 0 && scorePlayerB == 2) {
+            return "love thirty";
+        }
+        if (scorePlayerA == 0 && scorePlayerB == 3) {
+            return "love forty";
+        }
+        if (scorePlayerA == 1 && scorePlayerB == 1) {
+            return "fifteen all";
+        }
+        if (scorePlayerA == 2 && scorePlayerB == 2) {
+            return "thirty all";
+        }
+        if (scorePlayerA == 3 && scorePlayerB == 3) {
+            return "deuce";
+        }
+        if (scorePlayerA == 4 && scorePlayerB == 4) {
+            return "deuce";
+        }
+        if (scorePlayerA == 4 && scorePlayerB == 3) {
+            return "A adv";
+        }
+        if (scorePlayerA == 3 && scorePlayerB == 4) {
+            return "B adv";
+        }
+        if (scorePlayerA == 5 && scorePlayerB == 3) {
+            return "A win";
+        }
+        if (scorePlayerA == 3 && scorePlayerB == 5) {
+            return "B win";
+        }
+        return null;
+    }
 	
-	private String showScore(int cntScore) {
-		String scoreStr = "";
-		
-		if (cntScore == 0) {
-			scoreStr = "love";
-		} else if (cntScore == 15) {
-			scoreStr = "fifteen";
-		} else if (cntScore == 30) {
-			scoreStr = "thirty";
-		} else if (cntScore == 40) {
-			scoreStr = "forty";
-		}
-		return scoreStr;
-	}
+	public void playAAddScore() {
+        this.scorePlayerA++;
+    }
+
+    public void playBAddScore() {
+        this.scorePlayerB++;
+    }
 }
