59d7e3764908d4c35d489a940ce2d8a8ab42492e
Sawit Wongkasikij <sawit@summitthai.com>
2021-04-29 09:52:30 +0700

    init project

---
+++ src/main/java/com/scc/demo/tennis_legacy08/App.java
@@ -0,0 +1,13 @@
+package com.scc.demo.tennis_legacy08;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
